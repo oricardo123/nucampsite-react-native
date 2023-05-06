ede44886a9d7829a885a291fe996132a39407ef5
Ricardo <ricardoalmeida312@gmail.com>
2023-05-06 15:18:11 +0100

    task 2 workshop

---
+++ screens/CampsiteInfoScreen.js
@@ -0,0 +1,153 @@
+import React, { useState } from "react";
+import { FlatList, StyleSheet, Text, View, Button, Modal } from "react-native";
+import { useSelector, useDispatch } from "react-redux";
+import RenderCampsite from "../features/campsites/RenderCampsite";
+import { toggleFavorite } from "../features/favorites/favoritesSlice";
+import { Rating, Input } from "react-native-elements";
+import { FontAwesome } from "@expo/vector-icons";
+
+const CampsiteInfoScreen = ({ route }) => {
+  const { campsite } = route.params;
+  const comments = useSelector((state) => state.comments);
+  const favorites = useSelector((state) => state.favorites);
+  const dispatch = useDispatch();
+
+  const [showModal, setShowModal] = useState(false);
+  const [rating, setRating] = useState(5);
+  const [author, setAuthor] = useState("");
+  const [text, setText] = useState("");
+
+  const handleSubmit = () => {
+    const newComment = {
+      author,
+      rating,
+      text,
+      campsiteId: campsite.id,
+    };
+    console.log(newComment);
+    setShowModal(!showModal);
+  };
+
+  const resetForm = () => {
+    setRating(5);
+    setAuthor("");
+    setText("");
+  };
+
+  const renderCommentItem = ({ item }) => {
+    return (
+      <View style={styles.commentItem}>
+        <Text style={{ fontSize: 14 }}>{item.text}</Text>
+        <Rating
+          imageSize={10}
+          readonly
+          startingValue={item.rating}
+          style={{ alignItems: "flex-start", paddingVertical: "5%" }}
+        />
+        <Text
+          style={{ fontSize: 12 }}
+        >{`-- ${item.author}, ${item.date}`}</Text>
+      </View>
+    );
+  };
+
+  return (
+    <>
+      <FlatList
+        data={comments.commentsArray.filter(
+          (comment) => comment.campsiteId === campsite.id
+        )}
+        renderItem={renderCommentItem}
+        keyExtractor={(item) => item.id.toString()}
+        contentContainerStyle={{
+          marginHorizontal: 20,
+          paddingVertical: 20,
+        }}
+        ListHeaderComponent={
+          <>
+            <RenderCampsite
+              campsite={campsite}
+              isFavorite={favorites.includes(campsite.id)}
+              markFavorite={() => dispatch(toggleFavorite(campsite.id))}
+              onShowModal={() => setShowModal(!showModal)}
+            />
+            <Text style={styles.commentsTitle}>Comments</Text>
+          </>
+        }
+      />
+      <Modal
+        animationType={"slide"}
+        transparent={false}
+        visible={showModal}
+        onRequestClose={() => setShowModal(!showModal)}
+      >
+        <View style={styles.modal}>
+          <Rating
+            showRating
+            onFinishRating={(rating) => setRating(rating)}
+            startingValue={rating}
+            imageSize={40}
+            style={{ paddingVertical: 10 }}
+          />
+          <Input
+            placeholder="Author"
+            leftIcon={<FontAwesome name="user-o" size={24} color="black" />}
+            leftIconContainerStyle={{ paddingRight: 10 }}
+            onChangeText={(value) => setAuthor(value)}
+            value={author}
+          />
+          <Input
+            placeholder="Comment"
+            leftIcon={<FontAwesome name="comment-o" size={24} color="black" />}
+            leftIconContainerStyle={{ paddingRight: 10 }}
+            onChangeText={(value) => setText(value)}
+            value={text}
+          />
+          <View style={{ margin: 10 }}>
+            <Button
+              title="Submit"
+              color="#5637DD"
+              onPress={() => {
+                handleSubmit();
+                resetForm();
+              }}
+            />
+          </View>
+          <View style={{ margin: 10 }}>
+            <Button
+              onPress={() => {
+                setShowModal(!showModal);
+                resetForm();
+              }}
+              color="#808080"
+              title="Cancel"
+            />
+          </View>
+        </View>
+      </Modal>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  commentsTitle: {
+    textAlign: "center",
+    backgroundColor: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#43484D",
+    padding: 10,
+    paddingTop: 30,
+  },
+  commentItem: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    backgroundColor: "#fff",
+  },
+  modal: {
+    justifyContent: "center",
+    margin: 20,
+  },
+});
+
+export default CampsiteInfoScreen;
